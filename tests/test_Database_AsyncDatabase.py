9844c2dee50f5f9c3261e7f096675a92a7535b56
rmlibre@riseup.net <rmlibre@riseup.net>
2021-09-28 20:42:42 -0400

    v0.20.0: Major rewrite improving efficiency, readability, maintainability & security. See CHANGES.rst for more details.

--- tests/test_Database_AsyncDatabase.py
+++ tests/test_Database_AsyncDatabase.py
@@ -26,27 +26,27 @@ __all__ = [
 
 
 def test_Database_instance(database):
-    db = Database(key=key, password_depth=depth)
+    db = Database(key=key, depth=depth, preload=True)
 
     assert db._root_key == database._root_key
     assert db._root_hash == database._root_hash
     assert db._Database__root_salt == database._Database__root_salt
     assert db._root_seed == database._root_seed
     assert db._root_filename == database._root_filename
-    assert db.hmac(tag) == database.hmac(tag)
+    assert db.make_hmac(tag) == database.make_hmac(tag)
     assert db.filename(tag) == database.filename(tag)
     assert db._metatag_key(tag) == database._metatag_key(tag)
 
 
 def test_AsyncDatabase_instance(database):
-    db = run(AsyncDatabase(key=key, password_depth=depth))
+    db = run(AsyncDatabase(key=key, depth=depth, preload=True))
 
     assert db._root_key == database._root_key
     assert db._root_hash == database._root_hash
     assert db._AsyncDatabase__root_salt == database._Database__root_salt
     assert db._root_seed == database._root_seed
     assert db._root_filename == database._root_filename
-    assert run(db.ahmac(tag)) == database.hmac(tag)
+    assert run(db.amake_hmac(tag)) == database.make_hmac(tag)
     assert run(db.afilename(tag)) == database.filename(tag)
     assert run(db._ametatag_key(tag)) == database._metatag_key(tag)
 
@@ -58,11 +58,11 @@ def test_Database_cipher(database):
     encrypted_data = db.json_encrypt(test_data, filename=filename)
 
     db[tag] = test_data
-    db.save()
+    db.save_database()
     encrypted_file = db._query_ciphertext(filename)
 
     assert encrypted_file != encrypted_data
-    assert encrypted_file["salt"] != encrypted_data["salt"]
+    assert encrypted_file[SALT_SLICE] != encrypted_data[SALT_SLICE]
     assert db.json_decrypt(encrypted_file, filename=filename) == test_data
     assert db.json_decrypt(encrypted_data, filename=filename) == test_data
 
@@ -74,11 +74,11 @@ def test_AsyncDatabase_cipher(database, async_database):
     encrypted_data = run(db.ajson_encrypt(test_data, filename=filename))
 
     db[tag] = test_data
-    run(db.asave())
+    run(db.asave_database())
     encrypted_file = run(db._aquery_ciphertext(filename))
 
     assert encrypted_file != encrypted_data
-    assert encrypted_file["salt"] != encrypted_data["salt"]
+    assert encrypted_file[SALT_SLICE] != encrypted_data[SALT_SLICE]
     assert run(db.ajson_decrypt(encrypted_file, filename=filename)) == test_data
     assert run(db.ajson_decrypt(encrypted_data, filename=filename)) == test_data
 
@@ -113,10 +113,10 @@ def databases_share_metatags(db, adb, child, achild):
 
 
 def test_tags_metatags():
-    database = Database(key * 2, password_depth=depth)
-    async_database = run(AsyncDatabase(key * 2, password_depth=depth))
-    child = database.metatag(metatag)
-    achild = run(async_database.ametatag(ametatag))
+    database = Database(key * 2, depth=depth, preload=True)
+    async_database = run(AsyncDatabase(key * 2, depth=depth, preload=True))
+    child = database.metatag(metatag, preload=True)
+    achild = run(async_database.ametatag(ametatag, preload=True))
     databases_share_metatags(database, async_database, child, achild)
 
     child[tag] = test_data
@@ -129,8 +129,8 @@ def test_tags_metatags():
 
     metatag_key = database._metatag_key(metatag)
     ametatag_key = run(async_database._ametatag_key(ametatag))
-    db = Database(metatag_key, metatag=True)
-    adb = run(AsyncDatabase(ametatag_key, metatag=True))
+    db = Database(metatag_key, metatag=True, preload=True)
+    adb = run(AsyncDatabase(ametatag_key, metatag=True, preload=True))
     assert db[tag] == adb[atag]
     assert db[tag] == database.clients[tag]
     assert adb[atag] == async_database.aclients[atag]
@@ -147,7 +147,7 @@ async def async_user_profiles(async_database):
     async with user:
         user[atag] = atest_data
 
-    user_copy = await adb.aload_profile(tokens)
+    user_copy = await adb.aload_profile(tokens, preload=True)
     assert user[atag] == user_copy[atag]
     assert user[atag] is not user_copy[atag]
 
@@ -163,7 +163,7 @@ def user_profiles(database):
     with user:
         user[tag] = test_data
 
-    user_copy = db.load_profile(tokens)
+    user_copy = db.load_profile(tokens, preload=True)
     assert user[tag] == user_copy[tag]
     assert user[tag] is not user_copy[tag]
 
