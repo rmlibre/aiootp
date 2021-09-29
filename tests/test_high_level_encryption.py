bd41253f6f70b3a7c143fe740f72ed41b46f5165
rmlibre@riseup.net <rmlibre@riseup.net>
2021-09-28 20:41:28 -0400

    v0.20.0: Major rewrite improving efficiency, readability, maintainability & security. See CHANGES.rst for more details.

--- tests/test_high_level_encryption.py
+++ tests/test_high_level_encryption.py
@@ -21,34 +21,34 @@ __all__ = [
 
 
 def test_json_functions():
-    ciphertext_of_dict = json_encrypt(test_data, key, salt=salt, allow_dangerous_determinism=True)
-    ciphertext_of_string = json_encrypt(plaintext_string, key, salt=salt, allow_dangerous_determinism=True)
-    assert ciphertext_of_dict != run(ajson_encrypt(test_data, key, salt=salt, allow_dangerous_determinism=True))
-    assert ciphertext_of_string != run(ajson_encrypt(plaintext_string, key, salt=salt, allow_dangerous_determinism=True))
+    ciphertext_of_dict = json_encrypt(test_data, key, salt=salt)
+    ciphertext_of_string = json_encrypt(plaintext_string, key, salt=salt)
+    assert ciphertext_of_dict != run(ajson_encrypt(test_data, key, salt=salt))
+    assert ciphertext_of_string != run(ajson_encrypt(plaintext_string, key, salt=salt))
 
     assert ciphertext_of_dict != json_encrypt(test_data, key)
     assert ciphertext_of_string != run(ajson_encrypt(plaintext_string, key))
 
-    assert ciphertext_of_dict != json_encrypt(test_data, key, salt=salt, pid=pid, allow_dangerous_determinism=True)
-    assert ciphertext_of_string != run(ajson_encrypt(plaintext_string, key, salt=salt, pid=pid, allow_dangerous_determinism=True))
+    assert ciphertext_of_dict != json_encrypt(test_data, key, salt=salt, aad=aad)
+    assert ciphertext_of_string != run(ajson_encrypt(plaintext_string, key, salt=salt, aad=aad))
 
-    plaintext_of_dict = json_decrypt(json.dumps(ciphertext_of_dict), key)
-    plaintext_of_string = json_decrypt(json.dumps(ciphertext_of_string), key)
-    assert plaintext_of_dict == run(ajson_decrypt(json.dumps(ciphertext_of_dict), key))
-    assert plaintext_of_string == run(ajson_decrypt(json.dumps(ciphertext_of_string), key))
+    plaintext_of_dict = json_decrypt(ciphertext_of_dict, key)
+    plaintext_of_string = json_decrypt(ciphertext_of_string, key)
+    assert plaintext_of_dict == run(ajson_decrypt(ciphertext_of_dict, key))
+    assert plaintext_of_string == run(ajson_decrypt(ciphertext_of_string, key))
 
 
 def test_bytes_functions():
-    ciphertext_of_bytes = bytes_encrypt(plaintext_bytes, key, salt=salt, allow_dangerous_determinism=True)
-    ciphertext_of_string = bytes_encrypt(plaintext_string.encode(), key, salt=salt, allow_dangerous_determinism=True)
-    assert ciphertext_of_bytes != run(abytes_encrypt(plaintext_bytes, key, salt=salt, allow_dangerous_determinism=True))
-    assert ciphertext_of_string != run(abytes_encrypt(plaintext_string.encode(), key, salt=salt, allow_dangerous_determinism=True))
+    ciphertext_of_bytes = bytes_encrypt(plaintext_bytes, key, salt=salt)
+    ciphertext_of_string = bytes_encrypt(plaintext_string.encode(), key, salt=salt)
+    assert ciphertext_of_bytes != run(abytes_encrypt(plaintext_bytes, key, salt=salt))
+    assert ciphertext_of_string != run(abytes_encrypt(plaintext_string.encode(), key, salt=salt))
 
     assert ciphertext_of_bytes != bytes_encrypt(plaintext_bytes, key)
     assert ciphertext_of_string != run(abytes_encrypt(plaintext_string.encode(), key))
 
-    assert ciphertext_of_bytes != bytes_encrypt(plaintext_bytes, key, salt=salt, pid=pid, allow_dangerous_determinism=True)
-    assert ciphertext_of_string != run(abytes_encrypt(plaintext_string.encode(), key, salt=salt, pid=pid, allow_dangerous_determinism=True))
+    assert ciphertext_of_bytes != bytes_encrypt(plaintext_bytes, key, salt=salt, aad=aad)
+    assert ciphertext_of_string != run(abytes_encrypt(plaintext_string.encode(), key, salt=salt, aad=aad))
 
     plaintext_of_bytes = bytes_decrypt(ciphertext_of_bytes, key)
     plaintext_of_string = bytes_decrypt(ciphertext_of_string, key)
@@ -57,25 +57,25 @@ def test_bytes_functions():
 
 
 def test_token_functions():
-    ciphertext_of_bytes = pad.make_token(plaintext_bytes, pid=pid)
-    assert ciphertext_of_bytes != run(pad.amake_token(plaintext_bytes, pid=pid))
-    assert ciphertext_of_bytes != pad.make_token(plaintext_bytes)
-    assert ciphertext_of_bytes != run(pad.amake_token(plaintext_bytes))
-    assert ciphertext_of_bytes != pad.make_token(plaintext_bytes, key=salt, pid=pid)
-    assert ciphertext_of_bytes != run(pad.amake_token(plaintext_bytes, key=salt, pid=pid))
+    ciphertext_of_bytes = cipher.make_token(plaintext_bytes, aad=aad)
+    assert ciphertext_of_bytes != cipher.make_token(plaintext_bytes, aad=aad)
+    assert ciphertext_of_bytes != run(cipher.amake_token(plaintext_bytes, aad=aad))
+    assert ciphertext_of_bytes != cipher.make_token(plaintext_bytes)
+    assert ciphertext_of_bytes != run(cipher.amake_token(plaintext_bytes))
 
-    plaintext_of_bytes = pad.read_token(ciphertext_of_bytes, pid=pid)
-    assert plaintext_of_bytes == run(pad.aread_token(ciphertext_of_bytes, pid=pid))
+    plaintext_of_bytes = cipher.read_token(ciphertext_of_bytes, aad=aad)
+    assert plaintext_of_bytes == run(cipher.aread_token(ciphertext_of_bytes, aad=aad))
+    assert plaintext_bytes == plaintext_of_bytes
 
     asynchs.sleep(2)
     try:
-        pad.read_token(ciphertext_of_bytes, pid=pid, ttl=1)
+        cipher.read_token(ciphertext_of_bytes, aad=aad, ttl=1)
     except TimeoutError:
         pass
     else:
         raise AssertionError("Life-time for tokens is malfunctioning.")
     try:
-        run(pad.aread_token(ciphertext_of_bytes, pid=pid, ttl=1))
+        run(cipher.aread_token(ciphertext_of_bytes, aad=aad, ttl=1))
     except TimeoutError:
         pass
     else:
