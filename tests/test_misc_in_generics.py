ea060ccb7be9e6072bdaab26fbd0039ec7484ce8
rmlibre@riseup.net <rmlibre@riseup.net>
2021-09-28 20:40:53 -0400

    v0.20.0: Major rewrite improving efficiency, readability, maintainability & security. See CHANGES.rst for more details.

--- tests/test_misc_in_generics.py
+++ tests/test_misc_in_generics.py
@@ -9,11 +9,7 @@
 #
 
 
-__all__ = [
-    "__all__",
-    "test_Enumerate",
-    "test_binary_tree",
-]
+__all__ = ["__all__", "test_Enumerate"]
 
 
 from init_tests import *
@@ -22,57 +18,17 @@ from init_tests import *
 def test_Enumerate():
     async def async_iteration():
         zero_to_15 = cycle(range(16))
-        async for i, n in generics.Enumerate(iterable):
+        async for i, n in gentools.Enumerate(iterable):
             assert i == zero_to_15()
             assert n == iterable[i]
 
     def sync_iteration():
         zero_to_15 = cycle(range(16))
-        for i, n in generics.Enumerate(iterable):
+        for i, n in gentools.Enumerate(iterable):
             assert i == zero_to_15()
             assert n == iterable[i]
 
     iterable = "0123456789abcdef"
     run(async_iteration())
     sync_iteration()
-
-
-def test_binary_tree():
-    depth = randoms.unique_range(1, 6)
-    width = randoms.unique_range(1, 6)
-    leaf = bytes.fromhex(salt[:16])
-    kwargs = dict(depth=depth, width=width, leaf=leaf)
-
-    atree = run(generics.abuild_tree(**kwargs))
-    tree = generics.build_tree(**kwargs)
-
-    assert atree == tree
-
-    failing_kwargs_0 = dict(depth=-1, width=width, leaf=leaf)
-    try:
-        run(generics.abuild_tree(**failing_kwargs_0))
-    except:
-        pass
-    else:
-        raise AssertionError("Invalid depth accepted.")
-    try:
-        generics.build_tree(**failing_kwargs_0)
-    except:
-        pass
-    else:
-        raise AssertionError("Invalid depth accepted.")
-
-    failing_kwargs_1 = dict(depth=depth, width=0, leaf=leaf)
-    try:
-        run(generics.abuild_tree(**failing_kwargs_1))
-    except:
-        pass
-    else:
-        raise AssertionError("Invalid width accepted.")
-    try:
-        generics.build_tree(**failing_kwargs_1)
-    except:
-        pass
-    else:
-        raise AssertionError("Invalid width accepted.")
 
