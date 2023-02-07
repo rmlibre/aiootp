0b41428d1d43ccd8217add3e436ecb5c238b43c7
rmlibre <rmlibre@riseup.net>
2023-02-07 07:48:14 -0500

    v0.22.0: Cleanups to package UI & imports; declare public ed25519 & x25519 keys.

--- aiootp/__init__.py
+++ aiootp/__init__.py
@@ -4,18 +4,28 @@
 # Licensed under the AGPLv3: https://www.gnu.org/licenses/agpl-3.0.html
 # Copyright © 2019-2021 Gonzo Investigative Journalism Agency, LLC
 #            <gonzo.development@protonmail.ch>
-#           © 2019-2022 Richard Machado <rmlibre@riseup.net>
+#           © 2019-2023 Richard Machado <rmlibre@riseup.net>
 # All rights reserved.
 #
 
 
-__version__ = "0.21.3"
+__version__ = "0.22.0"
 
 
 __license__ = "AGPLv3"
 
 
 __author__ = "rmlibre@riseup.net"
+
+
+__PUBLIC_ED25519_KEY__ = (
+    "70d1740f2a439da98243c43a4d7ef1cf993b87a75f3bb0851ae79de675af5b3b"
+)
+
+
+__PUBLIC_X25519_KEY__ = (
+    "4457276dbcae91cc5b69f1aed4384b9eb6f933343bb44d9ed8a80e2ce438a450"
+)
 
 
 __doc__ = (
@@ -26,29 +36,52 @@ __doc__ = (
 
 from .commons import *
 from .paths import *
-from .asynchs  import *
-from .debuggers import *
+from .asynchs import *
+from .gentools import *
 from .generics import *
 from .randoms import *
 from .ciphers import *
 from .keygens import *
+from .databases import *
 from .__ui_coordination import *
 
 
 __all__ = [
-    *commons.__main_exports__,
-    *paths.__main_exports__,
-    *asynchs.__main_exports__,
-    *debuggers.__main_exports__,
-    *generics.__main_exports__,
-    *randoms.__main_exports__,
-    *ciphers.__main_exports__,
-    *keygens.__main_exports__,
+    "commons",
+    *commons.__all__,
+    *paths.__all__,
+    "asynchs",
+    *asynchs.__all__,
+    "gentools",
+    *gentools.__all__,
+    "generics",
+    *generics.__all__,
+    "randoms",
+    *randoms.__all__,
+    "ciphers",
+    *ciphers.__all__,
+    "keygens",
+    *keygens.__all__,
+    *databases.__all__,
 ]
 
 
-del __datasets
-del __async_lru
-del __aiocontext
+from .__engagement._report_security_issue import report_security_issue
+from ._debuggers import _debuggers
+from .commons import commons
+from .paths import paths as _paths
+from .asynchs  import asynchs
+from .gentools import gentools
+from .generics import generics
+from .randoms import randoms
+from .ciphers import ciphers
+from .keygens import keygens
+
+
+del paths
+del databases
+del __constants
+del __dependencies
+del __engagement
 del __ui_coordination
 
