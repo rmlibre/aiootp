355b21484c52a5d5ee87ca73662cf2efcf13368d
rmlibre <rmlibre@riseup.net>
2023-07-20 14:54:51 -0400

    v0.23.0: Prepare tests for the removal of top-level (a)json-, (a)bytes-, encrypt & decrypt functions. Instead, they'll be available directly from cipher objects.

--- tests/test_high_level_encryption.py
+++ tests/test_high_level_encryption.py
@@ -13,63 +13,63 @@ from test_initialization import *
 
 
 async def test_json_functions():
-    ciphertext_of_dict = json_encrypt(test_data, key, salt=salt)
-    ciphertext_of_string = json_encrypt(plaintext_string, key, salt=salt)
+    ciphertext_of_dict = cipher.json_encrypt(test_data, salt=salt)
+    ciphertext_of_string = cipher.json_encrypt(plaintext_string, salt=salt)
 
     # plaintext is not trivially detected in ciphertext
     assert json.dumps(test_data).encode() not in ciphertext_of_dict
     assert string_leakage not in ciphertext_of_string
 
     # no two ciphertexts are ever the same
-    assert ciphertext_of_dict != await ajson_encrypt(test_data, key, salt=salt)
-    assert ciphertext_of_string != await ajson_encrypt(plaintext_string, key, salt=salt)
+    assert ciphertext_of_dict != await cipher.ajson_encrypt(test_data, salt=salt)
+    assert ciphertext_of_string != await cipher.ajson_encrypt(plaintext_string, salt=salt)
 
-    assert ciphertext_of_dict != json_encrypt(test_data, key)
-    assert ciphertext_of_string != await ajson_encrypt(plaintext_string, key)
+    assert ciphertext_of_dict != cipher.json_encrypt(test_data)
+    assert ciphertext_of_string != await cipher.ajson_encrypt(plaintext_string)
 
-    assert ciphertext_of_dict != json_encrypt(test_data, key, salt=salt, aad=aad)
-    assert ciphertext_of_string != await ajson_encrypt(plaintext_string, key, salt=salt, aad=aad)
+    assert ciphertext_of_dict != cipher.json_encrypt(test_data, salt=salt, aad=aad)
+    assert ciphertext_of_string != await cipher.ajson_encrypt(plaintext_string, salt=salt, aad=aad)
 
     # decryption of correct ciphertext & key of json data doesn't fail
     # & is sound
-    plaintext_of_dict = json_decrypt(ciphertext_of_dict, key)
-    plaintext_of_string = json_decrypt(ciphertext_of_string, key)
+    plaintext_of_dict = cipher.json_decrypt(ciphertext_of_dict)
+    plaintext_of_string = cipher.json_decrypt(ciphertext_of_string)
     assert test_data == plaintext_of_dict
     assert plaintext_string == plaintext_of_string
 
     # async decryption with correct ciphertext & key of json data
     # doesn't fail & is sound
-    assert plaintext_of_dict == await ajson_decrypt(ciphertext_of_dict, key)
-    assert plaintext_of_string == await ajson_decrypt(ciphertext_of_string, key)
+    assert plaintext_of_dict == await cipher.ajson_decrypt(ciphertext_of_dict)
+    assert plaintext_of_string == await cipher.ajson_decrypt(ciphertext_of_string)
 
 
 async def test_bytes_functions():
-    ciphertext_of_bytes = bytes_encrypt(plaintext_bytes, key, salt=salt)
-    ciphertext_of_string = bytes_encrypt(plaintext_string.encode(), key, salt=salt)
+    ciphertext_of_bytes = cipher.bytes_encrypt(plaintext_bytes, salt=salt)
+    ciphertext_of_string = cipher.bytes_encrypt(plaintext_string.encode(), salt=salt)
 
     # plaintext is not trivially detected in ciphertext
     assert byte_leakage not in ciphertext_of_bytes
     assert string_leakage not in ciphertext_of_string
 
     # no two ciphertexts are ever the same
-    assert ciphertext_of_bytes != await abytes_encrypt(plaintext_bytes, key, salt=salt)
-    assert ciphertext_of_string != await abytes_encrypt(plaintext_string.encode(), key, salt=salt)
+    assert ciphertext_of_bytes != await cipher.abytes_encrypt(plaintext_bytes, salt=salt)
+    assert ciphertext_of_string != await cipher.abytes_encrypt(plaintext_string.encode(), salt=salt)
 
-    assert ciphertext_of_bytes != bytes_encrypt(plaintext_bytes, key)
-    assert ciphertext_of_string != await abytes_encrypt(plaintext_string.encode(), key)
+    assert ciphertext_of_bytes != cipher.bytes_encrypt(plaintext_bytes)
+    assert ciphertext_of_string != await cipher.abytes_encrypt(plaintext_string.encode())
 
-    assert ciphertext_of_bytes != bytes_encrypt(plaintext_bytes, key, salt=salt, aad=aad)
-    assert ciphertext_of_string != await abytes_encrypt(plaintext_string.encode(), key, salt=salt, aad=aad)
+    assert ciphertext_of_bytes != cipher.bytes_encrypt(plaintext_bytes, salt=salt, aad=aad)
+    assert ciphertext_of_string != await cipher.abytes_encrypt(plaintext_string.encode(), salt=salt, aad=aad)
 
     # decryption of correct ciphertext & key doesn't fail & is sound
-    plaintext_of_bytes = bytes_decrypt(ciphertext_of_bytes, key)
-    plaintext_of_string = bytes_decrypt(ciphertext_of_string, key)
+    plaintext_of_bytes = cipher.bytes_decrypt(ciphertext_of_bytes)
+    plaintext_of_string = cipher.bytes_decrypt(ciphertext_of_string)
     assert plaintext_bytes == plaintext_of_bytes
     assert plaintext_string == plaintext_of_string.decode()
 
     # async decryption with correct ciphertext & key doesn't fail & is sound
-    assert plaintext_of_bytes == await abytes_decrypt(ciphertext_of_bytes, key)
-    assert plaintext_of_string == await abytes_decrypt(ciphertext_of_string, key)
+    assert plaintext_of_bytes == await cipher.abytes_decrypt(ciphertext_of_bytes)
+    assert plaintext_of_string == await cipher.abytes_decrypt(ciphertext_of_string)
 
 
 async def test_token_functions():
