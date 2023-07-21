10f6f399ef6540199875f87f678b0949bd38bcfd
rmlibre <rmlibre@riseup.net>
2023-07-20 23:04:08 -0400

    v0.23.0: Move ciphers.py code to a subpackage to prepare for algorithm improvements & interface refactors that will also support new ciphers.

---
+++ aiootp/__ciphers/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of aiootp, an asynchronous crypto and anonymity
+# library. Home of the Chunky2048 psuedo one-time pad stream cipher.
+#
+# Licensed under the AGPLv3: https://www.gnu.org/licenses/agpl-3.0.html
+# Copyright © 2019-2021 Gonzo Investigative Journalism Agency, LLC
+#            <gonzo.development@protonmail.ch>
+#           © 2019-2023 Richard Machado <rmlibre@riseup.net>
+# All rights reserved.
+#
+
