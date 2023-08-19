969ab927690db96c82961bf035525f9a97c11ab4
rmlibre <rmlibre@riseup.net>
2023-08-19 01:06:26 -0400

    v0.23.0: Add more generalization for tests of slots/namespace classes. Add tests to be sure non-identifier names can be assigned as attributes to namespace classes.

--- tests/test_namespace_classes.py
+++ tests/test_namespace_classes.py
@@ -12,10 +12,15 @@
 from test_initialization import *
 
 
+IDENTIFIER_ITEMS = {"a": 0, "b": 1}
+NON_IDENTIFIER_ITEMS = {0: "a", 1: "b"}
+
+
 class SlotsExample(Slots):
     __slots__ = ("a", "b")
 
     class_variable: Typing.Any = "accessible"
+    ITEMS = IDENTIFIER_ITEMS
 
     async def aiter(self):
         return [name async for name in self]
@@ -24,37 +29,41 @@ class SlotsExample(Slots):
 class OpenNamespaceExample(OpenNamespace):
 
     class_variable: Typing.Any = "accessible"
+    ITEMS = IDENTIFIER_ITEMS
 
     async def aiter(self):
         return [name async for name in self]
 
 
 async def dunder_tests(cls, obj):
-    assert len(obj) == 2
+    assert len(obj) == len(cls.ITEMS)
     assert len(obj) == sum(1 for name in obj)
     assert len(obj) == sum(1 for name in await obj.aiter())
-    assert dir(obj) == ["a", "b"]
+    assert dir(obj) == list(cls.ITEMS)
     assert dir(obj) == [*obj]
     assert dir(obj) == [*obj.keys()]
     assert dir(obj) == await obj.aiter()
-    assert "a" in obj and "b" in obj
-    assert "a" not in cls() and "b" not in cls()
-    assert obj.a == 0
-    assert obj.a == obj["a"]
-    del obj["a"]
-    assert "a" not in obj
-    obj["a"] = 2
-    assert obj.a == 2
-    assert obj.a == obj["a"]
+    assert all((key in obj) for key in cls.ITEMS)
+    assert all((key not in cls()) for key in cls.ITEMS)
+    for name, value in cls.ITEMS.items():
+        assert name in obj
+        if type(name) is str:
+            assert getattr(obj, name) == value
+        assert obj[name] == value
+        del obj[name]
+        assert name not in obj
+        obj[name] = 2 * value
+        if type(name) is str:
+            assert getattr(obj, name) == 2 * value
+        assert obj[name] == 2 * value
     assert [obj[name] for name in obj] == [*obj.values()]
-    assert [getattr(obj, name) for name in obj] == [*obj.values()]
     assert [(name, obj[name]) for name in obj] == [*obj.items()]
     assert all(
-        (name in obj.__repr__(mask=False) and str(obj[name]) in obj.__repr__(mask=False))
+        (str(name) in obj.__repr__(mask=False) and str(obj[name]) in obj.__repr__(mask=False))
         for name in obj
     )
     assert all(
-        (name in obj.__repr__(mask=True) and str(obj[name]) not in obj.__repr__(mask=True))
+        (str(name) in obj.__repr__(mask=True) and str(obj[name]) not in obj.__repr__(mask=True))
         for name in obj
     )
     # debug mode toggles value viewability on and off
@@ -88,7 +97,7 @@ class TestSlots:
         Default Slots dunder methods don't fail when used.
         """
         cls = SlotsExample
-        obj = cls(a=0, b=1)
+        obj = cls(**IDENTIFIER_ITEMS)
         await dunder_tests(cls, obj)
 
 
@@ -98,14 +107,21 @@ class TestOpenNamespace:
         Default Slots dunder methods don't fail when used.
         """
         cls = OpenNamespaceExample
-        obj = cls(a=0, b=1)
+        obj = cls(**IDENTIFIER_ITEMS)
         await dunder_tests(cls, obj)
 
         # an object initialized with a mapping functions exactly the
         # same as initializing with keyword arguments
-        obj = cls(json.dumps(dict(a=0, b=1)))
+        obj = cls(json.dumps(dict(**IDENTIFIER_ITEMS)))
         await dunder_tests(cls, obj)
 
+        # an object functions correctly with non-identifier keys stored
+        # in __dict__
+        control_items = NON_IDENTIFIER_ITEMS.copy()
+        cls.ITEMS = NON_IDENTIFIER_ITEMS
+        obj = cls(NON_IDENTIFIER_ITEMS)
+        await dunder_tests(cls, obj)
+        assert control_items == cls.ITEMS
 
 
 __all__ = sorted({n for n in globals() if n.lower().startswith("test")})
