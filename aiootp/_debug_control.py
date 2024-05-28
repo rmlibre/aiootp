d0be781119785bb81cbf6b0884296b8f067a5e39
rmlibre <rmlibre@riseup.net>
2024-05-28 14:46:49 -0400

    v0.23.0: Refine the debuggers module into just the DebugControl class.

---
+++ aiootp/_debug_control.py
@@ -0,0 +1,72 @@
+# This file is part of aiootp:
+# an application agnostic — async-compatible — anonymity & cryptography
+# library, providing access to high-level Pythonic utilities to simplify
+# the tasks of secure data processing, communication & storage.
+#
+# Licensed under the AGPLv3: https://www.gnu.org/licenses/agpl-3.0.html
+# Copyright © 2019-2021 Gonzo Investigative Journalism Agency, LLC
+#            <gonzo.development@protonmail.ch>
+#           © 2019-2023 Richard Machado <rmlibre@riseup.net>
+# All rights reserved.
+#
+
+
+__all__ = ["DebugControl"]
+
+
+__doc__ = "A controller for toggling debug mode."
+
+
+import warnings
+from ._typing import Typing as t
+
+
+class DebugControl:
+    """
+    WARNING: Enabling debugging reveals potentially sensitive values,
+    -------- such as cryptographic keys, in object repr's that are
+    omitted by default. Also turns on asyncio's debugging.
+    """
+
+    __slots__ = ()
+
+    _DEBUG_MODE: bool = False
+
+    _switches: t.List[t.Callable[[bool], t.Any]] = []
+
+    @classmethod
+    def is_debugging(cls):
+        return cls._DEBUG_MODE
+
+    @classmethod
+    def enable_debugging(cls, *, silence_warnings: bool = False) -> None:
+        """
+        WARNING: This will reveal potentially sensitive values, such as
+        cryptographic keys, in object repr's that are omitted by default.
+        """
+        if not silence_warnings:
+            warnings.warn(cls.__doc__)  # pragma: no cover
+        if not cls._DEBUG_MODE:
+            cls._DEBUG_MODE = True
+            for toggle in cls._switches:
+                toggle()
+
+    @classmethod
+    def disable_debugging(cls) -> None:
+        if cls._DEBUG_MODE:
+            cls._DEBUG_MODE = False
+            for toggle in cls._switches:
+                toggle()
+
+
+module_api = dict(
+    DebugControl=DebugControl,
+    __all__=__all__,
+    __doc__=__doc__,
+    __file__=__file__,
+    __name__=__name__,
+    __spec__=__spec__,
+    __loader__=__loader__,
+    __package__=__package__,
+)
+
