40d3d0f996a6ce80b2418fadd1a25d9b1d4f9999
rmlibre <rmlibre@riseup.net>
2024-06-04 22:25:12 -0400

    v0.23.4: Refactor free-function test into a class.

--- tests/test_Padding.py
+++ tests/test_Padding.py
@@ -14,27 +14,31 @@
 from test_initialization import *
 
 
-def test_when_new_padding_blocks_are_created():
-    for cipher in [Chunky2048(key), Slick256(key)]:
-        config = cipher._config
-        Pad = Padding(config)
-        FIRST_BLOCKSIZE = (
-            config.BLOCKSIZE
-            - config.INNER_HEADER_BYTES
-            - config.SENTINEL_BYTES
-        )
+class TestPlaintextPadding:
 
-        start = config.INNER_HEADER_BYTES
-        for n in range(FIRST_BLOCKSIZE + config.BLOCKSIZE):
-            pt = n * b"0"
-            padded_pt = Pad.pad_plaintext(pt)
-            sentinel = int.from_bytes(padded_pt[config.SENTINEL_SLICE], BIG)
-            end = -(sentinel if sentinel else config.BLOCKSIZE)
+    async def test_variable_length_plaintexts(self) -> None:
+        for (config, cipher, _, _) in all_ciphers:
+            Pad = Padding(config)
+            assert config is Pad.config
+            assert config is cipher._padding.config
 
-            # The data is prepended with a constant size inner-header &
-            # appended with extra padding that is measured by the value of
-            # the final byte of padded plaintext modulo the blocksize.
-            assert padded_pt[start:end] == pt, f"n={n} : {sentinel=} : {start=} : {end=}"
+            FIRST_BLOCKSIZE = (
+                config.BLOCKSIZE
+                - config.INNER_HEADER_BYTES
+                - config.SENTINEL_BYTES
+            )
+
+            start = config.INNER_HEADER_BYTES
+            for n in range(FIRST_BLOCKSIZE + config.BLOCKSIZE):
+                pt = n * b"0"
+                padded_pt = Pad.pad_plaintext(pt)
+                sentinel = int.from_bytes(padded_pt[config.SENTINEL_SLICE], BIG)
+                end = -(sentinel if sentinel else config.BLOCKSIZE)
+
+                # The data is prepended with a constant size inner-header &
+                # appended with extra padding that is measured by the value of
+                # the final byte of padded plaintext modulo the blocksize.
+                assert padded_pt[start:end] == pt, f"{n=} : {sentinel=} : {start=} : {end=}"
 
 
 def test_chunky2048_min_padding_blocks_option():
